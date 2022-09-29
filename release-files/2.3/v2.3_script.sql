a3c58b3501960b76d95c0bac6417dec37c389408
nicolas <nicocamarao@gmail.com>
2022-09-29 10:02:59 -0300

    script 2.3 updated

--- release-files/2.3/v2.3_script.sql
+++ release-files/2.3/v2.3_script.sql
@@ -1,2 +1,7 @@
 ALTER TABLE public.uservoter ADD COLUMN version int4;
 UPDATE public.uservoter set version=1;
+INSERT INTO public.parameter
+(key, value)
+VALUES('AUDIT_REPORT_LINK', 'https://github.com/LACNIC/elections-open-source/blob/main/doc/auditReport.pdf');
+
+
